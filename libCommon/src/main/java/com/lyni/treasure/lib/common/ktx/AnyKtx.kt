7ab18d97d6154c803c51052de22a87fb1f5fcb06
toPolaris <toPolaris@163.com>
2022-05-16 14:36:33 +0800

    feat: 发布到jitpack配置

--- libCommon/src/main/java/com/lyni/treasure/lib/common/ktx/AnyKtx.kt
+++ libCommon/src/main/java/com/lyni/treasure/lib/common/ktx/AnyKtx.kt
@@ -3,6 +3,7 @@
 package com.lyni.treasure.lib.common.ktx
 
 import com.lyni.treasure.lib.common.utils.GsonUtil
+import kotlin.reflect.KClass
 
 /**
  * @date 2022/3/28
@@ -14,6 +15,8 @@ import com.lyni.treasure.lib.common.utils.GsonUtil
  * @return 深拷贝对象
  */
 inline fun <reified T : Any> T.deepCopy(): T = this.toJsonStr().jsonToObject(T::class.java)
+
+fun <T : Any> T.deepCopy(clz: KClass<T>): T = this.toJsonStr().jsonToObject(clz.java)
 
 /**
  * 将对象转成json格式
